041fd00b253b88f8d29486f22208e9c1695eab7f
Danboru <8danang8@gmail.com>
2017-08-14 23:14:26 +0700

    tambah fungsi cek internet jogjaOrganic

--- Jogja Organic/JogjaOrganic.swift
+++ Jogja Organic/JogjaOrganic.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SystemConfiguration
 
 class JogjaOrganic: UIViewController {
     
@@ -36,6 +37,31 @@ class JogjaOrganic: UIViewController {
  
     @IBAction func jogjaOrganicForward(_ sender: Any) {
         myWorldWebView.goForward()
+    }
+    
+    func connectedToNetwork() -> Bool {
+        
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout<sockaddr_in>.size)
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                SCNetworkReachabilityCreateWithAddress(nil, $0)
+            }
+        }) else {
+            return false
+        }
+        
+        var flags: SCNetworkReachabilityFlags = []
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
+            return false
+        }
+        
+        let isReachable = flags.contains(.reachable)
+        let needsConnection = flags.contains(.connectionRequired)
+        
+        return (isReachable && !needsConnection)
     }
     
 }
