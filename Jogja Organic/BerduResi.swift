6615e922b808ba8d535987976ffe8702c2776e38
Danboru <8danang8@gmail.com>
2017-08-14 23:05:32 +0700

    Tambah fungsi cek koneksi

--- Jogja Organic/BerduResi.swift
+++ Jogja Organic/BerduResi.swift
@@ -7,10 +7,10 @@
 //
 
 import UIKit
+import SystemConfiguration
 
 class BerduResi: UIViewController {
 
-    
     
     @IBOutlet weak var mySportWebView: UIWebView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
@@ -36,6 +36,31 @@ class BerduResi: UIViewController {
     
     @IBAction func berduForward(_ sender: Any) {
         mySportWebView.goForward()
+    }
+    
+    func connectedToNetwork() -> Bool {
+        
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout<sockaddr_in>.size)
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                SCNetworkReachabilityCreateWithAddress(nil, $0)
+            }
+        }) else {
+            return false
+        }
+        
+        var flags: SCNetworkReachabilityFlags = []
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
+            return false
+        }
+        
+        let isReachable = flags.contains(.reachable)
+        let needsConnection = flags.contains(.connectionRequired)
+        
+        return (isReachable && !needsConnection)
     }
     
 }
